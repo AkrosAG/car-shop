f13e520229636cfa04386241c3dfe0ec8d65c626
Tchoumi Steve <tchost@akros.ch>
2022-08-25 17:51:28 +0200

    start ngrx task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { AddAutoTeileComponentComponent } from './components/add-auto-teile-comp
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AutoTeileDetailEditComponent } from './components/auto-teile-detail-edit/auto-teile-detail-edit.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
@@ -26,6 +30,12 @@ import { AutoTeileDetailEditComponent } from './components/auto-teile-detail-edi
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent],
